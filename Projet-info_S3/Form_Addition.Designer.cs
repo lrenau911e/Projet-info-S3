fc4bfe25610db88ff94c4f31c1ced5620aaa97ff
ManonBonnet-Save <mbonnets@ensc.fr>
2016-11-21 11:45:41 +0100

    Programmation Addition
    Gestion de l'affichage des opérandes
    Gestion du résultat
    Affichage si OK ou non
    Gestion Score: Il ne manque plus que la réitération de formulaire

--- Projet-info_S3/Form_Addition.Designer.cs
+++ Projet-info_S3/Form_Addition.Designer.cs
@@ -28,28 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox_Operande1 = new System.Windows.Forms.TextBox();
-            this.textBox_Operande2 = new System.Windows.Forms.TextBox();
             this.textBox_Resultat = new System.Windows.Forms.TextBox();
             this.label_Resultat = new System.Windows.Forms.Label();
             this.button_Ok = new System.Windows.Forms.Button();
             this.label_Operateur = new System.Windows.Forms.Label();
             this.label_Titre = new System.Windows.Forms.Label();
+            this.label_Operande1 = new System.Windows.Forms.Label();
+            this.label_Operande2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // textBox_Operande1
-            // 
-            this.textBox_Operande1.Location = new System.Drawing.Point(13, 66);
-            this.textBox_Operande1.Name = "textBox_Operande1";
-            this.textBox_Operande1.Size = new System.Drawing.Size(135, 20);
-            this.textBox_Operande1.TabIndex = 0;
-            // 
-            // textBox_Operande2
-            // 
-            this.textBox_Operande2.Location = new System.Drawing.Point(15, 115);
-            this.textBox_Operande2.Name = "textBox_Operande2";
-            this.textBox_Operande2.Size = new System.Drawing.Size(135, 20);
-            this.textBox_Operande2.TabIndex = 1;
             // 
             // textBox_Resultat
             // 
@@ -57,6 +43,7 @@
             this.textBox_Resultat.Name = "textBox_Resultat";
             this.textBox_Resultat.Size = new System.Drawing.Size(202, 20);
             this.textBox_Resultat.TabIndex = 2;
+            this.textBox_Resultat.TextChanged += new System.EventHandler(this.textBox_Resultat_TextChanged);
             // 
             // label_Resultat
             // 
@@ -75,6 +62,7 @@
             this.button_Ok.TabIndex = 4;
             this.button_Ok.Text = "Ok";
             this.button_Ok.UseVisualStyleBackColor = true;
+            this.button_Ok.Click += new System.EventHandler(this.button_Ok_Click);
             // 
             // label_Operateur
             // 
@@ -94,20 +82,37 @@
             this.label_Titre.TabIndex = 6;
             this.label_Titre.Text = "Addition";
             // 
+            // label_Operande1
+            // 
+            this.label_Operande1.AutoSize = true;
+            this.label_Operande1.Location = new System.Drawing.Point(20, 77);
+            this.label_Operande1.Name = "label_Operande1";
+            this.label_Operande1.Size = new System.Drawing.Size(0, 13);
+            this.label_Operande1.TabIndex = 7;
+            // 
+            // label_Operande2
+            // 
+            this.label_Operande2.AutoSize = true;
+            this.label_Operande2.Location = new System.Drawing.Point(23, 110);
+            this.label_Operande2.Name = "label_Operande2";
+            this.label_Operande2.Size = new System.Drawing.Size(0, 13);
+            this.label_Operande2.TabIndex = 8;
+            // 
             // Form_Addition
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label_Operande2);
+            this.Controls.Add(this.label_Operande1);
             this.Controls.Add(this.label_Titre);
             this.Controls.Add(this.label_Operateur);
             this.Controls.Add(this.button_Ok);
             this.Controls.Add(this.label_Resultat);
             this.Controls.Add(this.textBox_Resultat);
-            this.Controls.Add(this.textBox_Operande2);
-            this.Controls.Add(this.textBox_Operande1);
             this.Name = "Form_Addition";
             this.Text = "Form_Addition";
+            this.Load += new System.EventHandler(this.Form_Addition_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,12 +120,12 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox_Operande1;
-        private System.Windows.Forms.TextBox textBox_Operande2;
         private System.Windows.Forms.TextBox textBox_Resultat;
         private System.Windows.Forms.Label label_Resultat;
         private System.Windows.Forms.Button button_Ok;
         private System.Windows.Forms.Label label_Operateur;
         private System.Windows.Forms.Label label_Titre;
+        private System.Windows.Forms.Label label_Operande1;
+        private System.Windows.Forms.Label label_Operande2;
     }
 }
