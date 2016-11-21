e2e5d5b33879dd9ae5fc27bf33ed60bdc335108d
Loïc_Renault <lrenau911e@ensc.fr>
2016-11-21 10:02:43 +0100

    Création formulaires loic

---
+++ Projet-info_S3/Problemes_Physiques.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Projet_info_S3
+{
+    public partial class Problemes_Physiques : Form
+    {
+        public Problemes_Physiques()
+        {
+            InitializeComponent();
+        }
+    }
+}
