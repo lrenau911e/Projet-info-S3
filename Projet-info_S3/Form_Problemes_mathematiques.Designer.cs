74f0afb15f9214a881b83fd19bdddd71b97f485e
ManonBonnet-Save <mbonnets@ensc.fr>
2016-11-28 14:57:39 +0100

    Update Form_Probleme_Mathematiques
    Création du Menu et disposition des boutons. Création du compteur et du
    score.

--- Projet-info_S3/Form_Problemes_mathematiques.Designer.cs
+++ Projet-info_S3/Form_Problemes_mathematiques.Designer.cs
@@ -28,11 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Problemes_mathematiques));
+            this.buttonCommencer = new System.Windows.Forms.Button();
+            this.textBox_Instruction = new System.Windows.Forms.TextBox();
+            this.checkBox_Rep1 = new System.Windows.Forms.CheckBox();
+            this.checkBox_Rep3 = new System.Windows.Forms.CheckBox();
+            this.checkBox_Rep2 = new System.Windows.Forms.CheckBox();
+            this.checkBox_Rep4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonCommencer
+            // 
+            this.buttonCommencer.Location = new System.Drawing.Point(80, 232);
+            this.buttonCommencer.Name = "buttonCommencer";
+            this.buttonCommencer.Size = new System.Drawing.Size(148, 23);
+            this.buttonCommencer.TabIndex = 0;
+            this.buttonCommencer.Text = "Commencer le test";
+            this.buttonCommencer.UseVisualStyleBackColor = true;
+            this.buttonCommencer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_Instruction
+            // 
+            this.textBox_Instruction.Location = new System.Drawing.Point(33, 12);
+            this.textBox_Instruction.Multiline = true;
+            this.textBox_Instruction.Name = "textBox_Instruction";
+            this.textBox_Instruction.ReadOnly = true;
+            this.textBox_Instruction.Size = new System.Drawing.Size(268, 99);
+            this.textBox_Instruction.TabIndex = 1;
+            this.textBox_Instruction.Text = resources.GetString("textBox_Instruction.Text");
+            // 
+            // checkBox_Rep1
+            // 
+            this.checkBox_Rep1.AutoSize = true;
+            this.checkBox_Rep1.Location = new System.Drawing.Point(57, 149);
+            this.checkBox_Rep1.Name = "checkBox_Rep1";
+            this.checkBox_Rep1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox_Rep1.TabIndex = 4;
+            this.checkBox_Rep1.Text = "checkBox1";
+            this.checkBox_Rep1.UseVisualStyleBackColor = true;
+            this.checkBox_Rep1.Visible = false;
+            this.checkBox_Rep1.CheckedChanged += new System.EventHandler(this.checkBox_Rep1_CheckedChanged);
+            // 
+            // checkBox_Rep3
+            // 
+            this.checkBox_Rep3.AutoSize = true;
+            this.checkBox_Rep3.Location = new System.Drawing.Point(57, 186);
+            this.checkBox_Rep3.Name = "checkBox_Rep3";
+            this.checkBox_Rep3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox_Rep3.TabIndex = 5;
+            this.checkBox_Rep3.Text = "checkBox2";
+            this.checkBox_Rep3.UseVisualStyleBackColor = true;
+            this.checkBox_Rep3.Visible = false;
+            // 
+            // checkBox_Rep2
+            // 
+            this.checkBox_Rep2.AutoSize = true;
+            this.checkBox_Rep2.Location = new System.Drawing.Point(182, 149);
+            this.checkBox_Rep2.Name = "checkBox_Rep2";
+            this.checkBox_Rep2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox_Rep2.TabIndex = 6;
+            this.checkBox_Rep2.Text = "checkBox3";
+            this.checkBox_Rep2.UseVisualStyleBackColor = true;
+            this.checkBox_Rep2.Visible = false;
+            // 
+            // checkBox_Rep4
+            // 
+            this.checkBox_Rep4.AutoSize = true;
+            this.checkBox_Rep4.Location = new System.Drawing.Point(182, 186);
+            this.checkBox_Rep4.Name = "checkBox_Rep4";
+            this.checkBox_Rep4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox_Rep4.TabIndex = 7;
+            this.checkBox_Rep4.Text = "checkBox4";
+            this.checkBox_Rep4.UseVisualStyleBackColor = true;
+            this.checkBox_Rep4.Visible = false;
+            // 
+            // Form_Problemes_mathematiques
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 266);
+            this.Controls.Add(this.checkBox_Rep4);
+            this.Controls.Add(this.checkBox_Rep2);
+            this.Controls.Add(this.checkBox_Rep3);
+            this.Controls.Add(this.checkBox_Rep1);
+            this.Controls.Add(this.textBox_Instruction);
+            this.Controls.Add(this.buttonCommencer);
+            this.Name = "Form_Problemes_mathematiques";
             this.Text = "Form_Problemes_mathematiques";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonCommencer;
+        private System.Windows.Forms.TextBox textBox_Instruction;
+        private System.Windows.Forms.CheckBox checkBox_Rep1;
+        private System.Windows.Forms.CheckBox checkBox_Rep3;
+        private System.Windows.Forms.CheckBox checkBox_Rep2;
+        private System.Windows.Forms.CheckBox checkBox_Rep4;
     }
 }
--- Projet-info_S3/Form_Problemes_mathematiques.cs
+++ Projet-info_S3/Form_Problemes_mathematiques.cs
@@ -11,9 +11,71 @@ namespace Projet_info_S3
 {
     public partial class Form_Problemes_mathematiques : Form
     {
+        int NombrePb = 1;
+        int Succes = 0;
+
         public Form_Problemes_mathematiques()
         {
             InitializeComponent();
+        }
+
+        private void AfficheResultat()
+        {
+            Succes = Succes * 10;
+            string score = "Voici votre résultat: \n" + Succes + " %";
+            MessageBox.Show(score, "Score");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox_Instruction.Text = ""; //TODO: relier à la base de donnée
+            buttonCommencer.Text = "Problème Suivant : " + NombrePb.ToString() + "/10";
+            checkBox_Rep1.Visible = true;
+            checkBox_Rep1.Text = "";//TODO: relier à la base de donnée
+            checkBox_Rep2.Visible = true;
+            checkBox_Rep2.Text = "";//TODO: relier à la base de donnée
+            checkBox_Rep3.Visible = true;
+            checkBox_Rep3.Text = "";//TODO: relier à la base de donnée
+            checkBox_Rep4.Visible = true;
+            checkBox_Rep4.Text = "";//TODO: relier à la base de donnée
+
+            /*if (checkBox_Rep1.Checked == true)
+            {
+                checkBox_Rep2.Enabled = false;
+                checkBox_Rep3.Enabled = false;
+                checkBox_Rep4.Enabled = false;
+            }
+
+            if (checkBox_Rep2.Checked == true)
+            {
+                checkBox_Rep1.Enabled = false;
+                checkBox_Rep3.Enabled = false;
+                checkBox_Rep4.Enabled = false;
+            }
+
+            if (checkBox_Rep3.Checked == true)
+            {
+                checkBox_Rep1.Enabled = false;
+                checkBox_Rep2.Enabled = false;
+                checkBox_Rep4.Enabled = false;
+            }
+
+            if (checkBox_Rep4.Checked == true)
+            {
+                checkBox_Rep1.Enabled = false;
+                checkBox_Rep2.Enabled = false;
+                checkBox_Rep3.Enabled = false;
+            }*/
+
+            NombrePb++;
+            if (NombrePb == 11)
+            {
+                buttonCommencer.Text = "Résultat";
+                AfficheResultat();
+                NombrePb++;
+            }
+            if (NombrePb == 12)
+                this.Close();
         }
     }
 }
